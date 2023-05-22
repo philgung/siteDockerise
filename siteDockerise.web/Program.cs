4e6eb9637c713ab90b9c4507b8023a6bfe6bb574
philgung <phil.gung@gmail.com>
2023-05-22 14:46:36 +0200

    ajout swagger

--- siteDockerise.web/Program.cs
+++ siteDockerise.web/Program.cs
@@ -1,6 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+// Postgresql & Kafka
+// Afficher messages
+// Créer message
+// Mettre à jour message
 
-app.MapGet("/", () => "Hello World!");
+app.UseSwagger();
 
+app.MapGet("/", () => "Hello World!")
+    .WithName("Hello");
+app.UseSwaggerUI();
 app.Run();
